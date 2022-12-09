54ffd5262869bfe36f5e9ab042d69e4f9802021b
a3ela <abelsintayehu626@gmail.com>
2022-12-09 22:14:26 +0300

    done task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print all numbers of base64 in lowercase.
+ * You can only use `putchar`
+ * Return: 0
+ */
+
+int main(void)
+{
+do
+	char alp='a';
+	printf("%c",alp);
+
+while(alp <=z)
+
+	return (0);
+
+}
