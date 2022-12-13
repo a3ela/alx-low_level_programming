55e742abcfbb4a8c7491e4e3c158fa7a405f9557
a3ela <abelsintayehu626@gmail.com>
2022-12-13 17:27:54 +0300

    fixed betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,12 @@
 #include "main.h"
 /**
- *main - Entry point
- *Description: print alphabet 10 times
- *Return: 0
+ * print_alphabet_x10 - print lowercase alphabet ten times
+ * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	char alpha;
 	int i;
-	/* initialization */
-	
 
 	for (i = 0; i < 10; i++)
 	{
