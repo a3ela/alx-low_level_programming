3154a1aeb9bbdd08e19e20f8c77de715c8edbf17
a3ela <abelsintayehu626@gmail.com>
2022-12-15 22:36:46 +0300

    done task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
+int _isupper(int c)
+{
+	while (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
