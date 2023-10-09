68970df92d41ee7a2ba7b491848d2ff7d51e5a6a
a3ela <abelsintayehu626@gmail.com>
2023-10-09 23:59:31 +0300

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print numbers of base 10
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+		printf("%i", i);
+	putchar('\n');
+	return (0);
+}
