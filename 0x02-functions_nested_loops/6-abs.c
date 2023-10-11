ccf117354a6cedcb0ccd6f557b223f4272be4bdb
a3ela <abelsintayehu626@gmail.com>
2023-10-11 14:40:18 +0300

    return absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - compute absolute value
+ * @n: the number to be converted
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return(n * -1);
+	else
+		return (n);
+}
