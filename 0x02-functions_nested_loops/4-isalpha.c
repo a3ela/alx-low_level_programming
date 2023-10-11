e68ba6bb13c4ad5d5b4f97ee7ef25e7a7d04141f
a3ela <abelsintayehu626@gmail.com>
2023-10-11 13:50:27 +0300

    func check alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * isalpha - checks alphabets
+ * @c: the character
+ * Return: 0(success)
+ */
+int _isalpha(int c)
+{
+	char low_alpha = 'a';
+	char up_alpha = 'A';
+
+	while (low_alpha <= 'z')
+	{
+		if (c == low_alpha)
+			return (1);
+		if (c == up_alpha)
+			return (1);
+		low_alpha++;
+		up_alpha++;
+	}
+	return (0);
+}
