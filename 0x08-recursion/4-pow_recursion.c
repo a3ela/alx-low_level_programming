71a7a834365f2fcfb1b7ffe85d897761f86023e3
a3ela <abelsintayehu626@gmail.com>
2023-10-26 15:56:42 +0300

    power of x and y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _pow_recursion - x raised power of y
+ * @x: value 1
+ * @y: value 2
+ * Return: value
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(int x, y - 1));
+	}
+}
