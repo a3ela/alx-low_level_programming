0ac83ab30bb27083131b0e446963c109ab64cbaa
a3ela <abelsintayehu626@gmail.com>
2023-11-27 18:04:49 +0300

    function of strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: strign to be searched
+ * @needle: substring to be found
+ * Return: pointer to substring or NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *start = haystack;
+	char *_needle = needle;
+	bool found = false;
+
+	if (!*_needle)
+		return (haystack);
+
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+		{
+			found = true;
+			start = haystack;
+			while (*_needle)
+			{
+				if (*haystack != *_needle)
+				{
+					found = false;
+					_needle = needle;
+					break;
+				}
+				haystack++;
+				_needle++;
+			}
+		}
+		if (found)
+		{
+			break;
+		}
+		haystack++;
+	}
+	if (found)
+	{
+		return (start);
+	}
+	return (NULL);
+}
