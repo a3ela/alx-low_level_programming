8ca026eeb6e03f7bb4b9105c6a3689d701bfb3ae
a3ela <abelsintayehu626@gmail.com>
2023-11-29 17:56:08 +0300

    fixed betty problem

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -5,7 +5,8 @@
  * malloc_checked - Allocates memory using malloc, but not if malloc failure,
  * it causes normal process termination with a status value of 98
  * @b: The size of memory to allocate
- * Return: the pointer to the allocated memory if successfull, otherwise / program exits
+ * Return: the pointer to the allocated memory if successfull, otherwise
+ * / program exits
  */
 void *malloc_checked(unsigned int b)
 {
